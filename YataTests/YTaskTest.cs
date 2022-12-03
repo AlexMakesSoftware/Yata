029146d36de52c1797826bfc23e1dde3d25de605
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2022-12-03 20:32:35 +0000

    adding initial tests and project config

--- YataTests/YTaskTest.cs
+++ YataTests/YTaskTest.cs
@@ -7,6 +7,21 @@ public class YTaskTest
     public void TestTaskStartsAsUnsorted()
     {
         YTask t = new YTask("Write Yata - Yet Another Todo App.");        
-        Assert.Equal(t.State, TaskState.Unsorted);
+        Assert.Equal(TaskState.Unsorted, t.State);
+    }
+
+    [Fact]
+    public void TestLoadTasks() {
+        List<YTask> loaded = TaskListLoader.LoadTasks(".\resources\testJobs.json");
+        Assert.Equal(3,loaded.Count);
+    }
+
+    [Fact]
+    public void TestSaveTasks() {
+        List<YTask> todo = new List<YTask>();
+        todo.Add(new YTask("one"));
+        todo.Add(new YTask("two"));
+        todo.Add(new YTask("three"));
+        TaskListLoader.SaveTasks(todo);
     }
 }
