276cbfc00fd951cc8dfc26e6cd7538ed566cf50c
Alex Connell <83471521+AlexMakesSoftware@users.noreply.github.com>
2022-12-07 17:50:50 +0000

    added command line

--- Yata.Cmd/Program.cs
+++ Yata.Cmd/Program.cs
@@ -1,73 +1,128 @@
 ﻿using YataModel;
 using YataModel.Storage;
 using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
 
 public class Program
-{
-    private static void Main(string[] args)
-    {
-        //print to console in color
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Welcome to Yet Another TODO App!");
-        Console.ForegroundColor = ConsoleColor.White;
+{       
 
-        IStorage store = new FileStoreImpl();
-        store.LoadTodos();
+    private static string _banner = @"
+░░    ░░  ░░░░░  ░░░░░░░░  ░░░░░    
+ ▒▒  ▒▒  ▒▒   ▒▒    ▒▒    ▒▒   ▒▒ 
+  ▒▒▒▒   ▒▒▒▒▒▒▒    ▒▒    ▒▒▒▒▒▒▒ 
+   ▓▓    ▓▓   ▓▓    ▓▓    ▓▓   ▓▓ 
+   ██    ██   ██    ██    ██   ██ v0.0.1";    
 
-        //sort tasks by due date
-        //
+    private static IStorage _store = new FileStoreImpl();
 
-        List<YTask> todaysJobs = store.JobsDueToday();
+    //test with  dotnet run add --task "This is a test" --due 2021-01-01
 
-        //print out today's jobs
-        Console.WriteLine("Today's Jobs:");
-        //if todaysJobs is empty, print out "No jobs scheduled for today"
-        if (todaysJobs.Count == 0)
-        {
-            Console.WriteLine("No jobs scheduled for today");
-        }
-        else 
-        {
-            //printout todaysjobs list
-            foreach (YTask task in todaysJobs)
-            {
-                Console.WriteLine(task.Description);
-            }
-        }
+    [Verb("add", HelpText = "Add a new task to the list.")]
+    class AddOptions {
+        //options for adding a task
+        [Option(Group = "add", Required = true, HelpText = "The description of the task.")]
+        public string? Task { get; set; }
 
-        //make this pink
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        //print list of jobs that are overdue
-        Console.WriteLine("Overdue Jobs:");
-        //if overdueJobs is empty, print out "No jobs overdue"
-        if (store.OverdueJobs().Count == 0)
-        {
-            Console.WriteLine("No jobs overdue");
-        }
-        else
-        {
-            //printout overdueJobs list
-            foreach (YTask task in store.OverdueJobs())
-            {
-                Console.WriteLine(task.Description);
-            }
-        }
+        [Option(Group = "add", HelpText = "The due date of the task. In the format YYYY-MM-DD.")]
+        public DateTime? Due { get; set; }
+    }
+
+    //Q:What's the dotnet command to run this with arguments for main?
+    //A: dotnet run -- add -d "Do the dishes" -D 2021-01-01
 
 
-        //stats
-        int totalUnsorted = store.CountByState(TaskState.Unsorted);
-        int tickler = store.CountByState(TaskState.Tickler);
-        int completed = store.CountByState(TaskState.Completed);
-
-        //print out stats
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine("Stats:");
-        Console.WriteLine($"Total Unsorted: {totalUnsorted}");
-        Console.WriteLine($"Total Tickler: {tickler}");
-        Console.WriteLine($"Total Completed: {completed}");
-
+    [Verb("due", HelpText = "See the list of tasks that are due to be completed now (or earlier).")]
+    class DueOptions {
 
     }
 
-   
+    private static void Main(string[] args)
+    {
+        //Q;What's the best way to handle command-line switches?
+        //A: Use a library like CommandLineParser
+        
+        _store.LoadTodos();
+
+  var parser = new CommandLine.Parser(with => with.HelpWriter = null);
+  var parserResult = parser.ParseArguments<AddOptions,DueOptions>(args);
+  parserResult
+    .WithParsed<AddOptions>(options => RunAddAndReturnExitCode(options))
+    .WithParsed<DueOptions>(options => RunDueAndReturnExitCode(options))
+    .WithNotParsed(errs => DisplayHelp(parserResult, errs));
+
+
+
+		// return CommandLine.Parser.Default.ParseArguments<AddOptions,DueOptions>(args)
+	    //     .MapResult(
+        //         (AddOptions opts) => RunAddAndReturnExitCode(opts),
+        //         (DueOptions opts) => RunDueAndReturnExitCode(opts),                
+        //         errs => 1);
+    }
+
+    private static void DisplayHelp(ParserResult<object> result, IEnumerable<Error> errs)
+    {
+        var helpText = HelpText.AutoBuild(result, h =>
+        {
+            h.AdditionalNewLineAfterOption = false;
+            h.Heading = _banner;
+            h.Copyright = "Freeware (c) 2022 Alex Connell"; //TODO: update this
+            return HelpText.DefaultParsingErrorsHandler(result, h);
+        }, e => e);        
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(helpText);        
+        Console.ResetColor();
+        Console.WriteLine("Example: yata add --task \"Washing up.\"");
+    }
+
+    private static int RunDueAndReturnExitCode(DueOptions opts)
+    {
+        Console.WriteLine("Outstanding tasks due now (or earlier):");
+        
+        //get the list of tasks due now
+        List<YTask> dueTasks = _store.DueJobs();
+        //sort it by due date
+        dueTasks.Sort((x,y) => x.Due.CompareTo(y.Due));
+        //print it out
+        foreach (YTask task in dueTasks)
+        {
+            Console.WriteLine(task.ToString());
+        }        
+
+        return 0;
+    }
+
+    //Q: How do I run this to add a task?
+    //A: dotnet run add --task "This is a test" --due 2021-01-01
+
+    private static int RunAddAndReturnExitCode(AddOptions opts)
+    {
+        //Throw exception if opts.Task is null
+        if (opts.Task == null)
+        {
+            throw new System.ArgumentNullException("No task description speicified!");
+        }
+
+        Console.WriteLine("Adding a new task to the list:"+opts);
+        
+        //Add a new task to the list
+        YTask task = new YTask(opts.Task);        
+        
+        //if opts.Due is set, split it into a date and time
+        if (opts.Due.HasValue)
+        {
+            task.Due = opts.Due.Value;
+        }
+        else
+        {
+            task.Due = DateTime.Now;
+        }
+
+        //add the task to the list
+        _store.AddTask(task);
+        //save the list
+        _store.SaveTodos();
+
+        return 0;
+    }
 }
